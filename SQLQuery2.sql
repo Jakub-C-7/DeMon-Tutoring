0cb36be0b450b4b91ecab63c7f98f876b2de6f4e
Jakub Chamera <58332431+Jakub-C-7@users.noreply.github.com>
2021-10-06 23:52:25 +0100

    Added the full main application for the project

---
+++ SQLQuery2.sql
@@ -0,0 +1,15 @@
+﻿USE [DeMon_Tutoring_Db]
+GO
+
+DECLARE	@return_value Int
+
+EXEC	@return_value = [dbo].[sproc_TblStaffing_InsertNotNulls]
+		@firstName = N'Eugine',
+		@lastName = N'Zuccerberg',
+		@staffEmail = N'eugenefrisbee@gmail.com',
+		@staffNumber = N'07974133370',
+		@staffDOB = N'1000-08-09'
+
+SELECT	@return_value as 'Return Value'
+
+GO
---
+++ dbo.tblCustomer.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[tblCustomer] (
+    [CustomerID]    INT          NOT NULL,
+    [FirstName]     VARCHAR (25) NOT NULL,
+    [LastName]      VARCHAR (25) NOT NULL,
+    [DateOfBirth]   DATE         NOT NULL,
+    [Email]         VARCHAR (30) NOT NULL,
+    [PhoneNumber]   VARCHAR (11) NULL,
+    [Password]      VARCHAR (25) NOT NULL,
+    [CardNo]        VARCHAR (16) NOT NULL,
+    [CardDate]      DATE         NOT NULL,
+    [StudentStatus] BIT          NOT NULL,
+    PRIMARY KEY CLUSTERED ([CustomerID] ASC)
+);
+
